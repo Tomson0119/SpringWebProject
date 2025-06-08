a54b77823c38e767da6863c26c929d3e73351878
wltjd666 <kjs980119@gmail.com>
2025-06-08 21:51:57 +0900

    ### multi module test  - common 모듈 추가  - api 모듈 추가  - root gradle에서 추가한 의존 라이브러리를 하위 모듈에서도 참조 가능하게 subproject 적용  - api 모듈에서는 common 모듈을 참조하도록 설정

--- api/src/main/java/com/project/api/WebProjectApplication.java
+++ api/src/main/java/com/project/api/WebProjectApplication.java
@@ -1,4 +1,4 @@
-package com.project.web_project;
+package com.project.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
