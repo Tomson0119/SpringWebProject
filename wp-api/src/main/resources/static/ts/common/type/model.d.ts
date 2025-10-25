4cfee9d06d49e9f81448a8681dc2f7c91f48cf68
wltjd666 <kjs980119@gmail.com>
2025-10-25 15:31:13 +0900

    ### Swagger를 통해 TS 모델 자동 생성  - DTO에 정의된 LoginRequest/Response 생성

---
+++ wp-api/src/main/resources/static/ts/common/type/model.d.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-nocheck
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface LoginRequest {
+  name?: string;
+  password?: string;
+}
+
+export interface LoginResponse {
+  name?: string;
+  password?: string;
+}
