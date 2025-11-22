d1764a29470e1625bf6dd903310426aa0ded3701
wltjd666 <kjs980119@gmail.com>
2025-11-22 11:55:14 +0900

    ### MySql 및 JPA 세팅

--- wp-app/src/main/java/com/project/wp_app/WebProjectApplication.java
+++ wp-app/src/main/java/com/project/wp_app/WebProjectApplication.java
@@ -2,10 +2,12 @@ package com.project.wp_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = { "com.project.wp_api" })
+@SpringBootApplication(scanBasePackages = {"com.project"})
+@EnableJpaRepositories(basePackages = {"com.project.wp_domain.repository"})
+@EntityScan(basePackages = {"com.project.wp_domain.entity"})
 public class WebProjectApplication {
 
     public static void main(String[] args) {
