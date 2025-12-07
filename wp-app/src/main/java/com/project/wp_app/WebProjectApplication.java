c8e9ae3d1f29f624699b69012b40c0d48eb3d5d9
wltjd666 <kjs980119@gmail.com>
2025-12-07 15:37:13 +0900

    ### Security 인증 과정 제거  - security 모듈에 의해 접속 시 자동으로 인증을 요구하는 과정을 제거함

--- wp-app/src/main/java/com/project/wp_app/WebProjectApplication.java
+++ wp-app/src/main/java/com/project/wp_app/WebProjectApplication.java
@@ -3,13 +3,13 @@ package com.project.wp_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.project"})
+@SpringBootApplication(scanBasePackages = {"com.project"}, exclude = SecurityAutoConfiguration.class)
 @EnableJpaRepositories(basePackages = {"com.project.wp_domain.repository"})
 @EntityScan(basePackages = {"com.project.wp_domain.entity"})
 public class WebProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebProjectApplication.class, args);
     }
