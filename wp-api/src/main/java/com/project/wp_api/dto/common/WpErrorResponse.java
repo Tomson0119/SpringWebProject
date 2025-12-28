e05efbe7405a4acfcf9019d3dc8cea5db5977392
wltjd666 <kjs980119@gmail.com>
2025-12-28 14:30:01 +0900

    ### Response에 Getter 추가  - 스프링에서 Get 메서드를 필요로 하여 꼭 붙여야 함  - 붙이지 않고 전송하는 경우, front에서 제대로 파싱할 수 없음

--- wp-api/src/main/java/com/project/wp_api/dto/common/WpErrorResponse.java
+++ wp-api/src/main/java/com/project/wp_api/dto/common/WpErrorResponse.java
@@ -2,8 +2,10 @@ package com.project.wp_api.dto.common;
 
 import com.project.wp_api.dto.common.enums.CustomErrorCode;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
+@Getter
 @Setter
 @AllArgsConstructor
 public class WpErrorResponse extends WpResponse {
--- wp-api/src/main/java/com/project/wp_api/dto/member/FindMemberResponse.java
+++ wp-api/src/main/java/com/project/wp_api/dto/member/FindMemberResponse.java
@@ -1,10 +1,14 @@
 package com.project.wp_api.dto.member;
 
 import com.project.wp_api.dto.common.WpResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Getter
 @Setter
+@RequiredArgsConstructor
 public final class FindMemberResponse extends WpResponse {
-    private Long memberId;
-    private String memberName;
+    private final Long memberId;
+    private final String memberName;
 }
