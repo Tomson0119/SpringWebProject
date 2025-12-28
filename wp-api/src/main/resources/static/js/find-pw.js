fe2b4dca6e6dd2691860756505fe19a36db5399e
wltjd666 <kjs980119@gmail.com>
2025-12-28 16:45:33 +0900

    ### 비밀번호 찾기 기능 작업 (Front)  - 아이디를 입력받아 비밀번호 찾기 API 전송  - 아이디가 유효하면 입력 칸, 버튼은 비활성화

---
+++ wp-api/src/main/resources/static/js/find-pw.js
@@ -0,0 +1,41 @@
+import { CustomErrorCode } from "./common/type/model.js";
+import { getElement } from "./common/utility/htmlUtility.js";
+import { get, isWpErrorResponse } from "./common/utility/httpUtility.js";
+async function sendFindPwRequest() {
+    const input_name = getElement("input-name");
+    const success_message = getElement("success-message");
+    const invalid_name_error = getElement("invalid-name-error");
+    const response = await get("/members/find-pw", { name: input_name.value });
+    if (isWpErrorResponse(response)) {
+        const wpErrorResponse = await response.json();
+        console.error(`Failed to find pw: ${wpErrorResponse.customErrorCode}`);
+        if (wpErrorResponse.customErrorCode == CustomErrorCode.MEMBER_NOT_FOUND) {
+            invalid_name_error.style.display = "flex";
+            success_message.style.display = "none";
+            input_name.value = "";
+        }
+        return;
+    }
+    invalid_name_error.style.display = "none";
+    success_message.style.display = "flex";
+    cleanup();
+}
+function cleanup() {
+    const input_name = getElement("input-name");
+    const find_button = getElement("find-pw-button");
+    input_name.disabled = true;
+    find_button.disabled = true;
+}
+function main() {
+    // input_name 엔터 입력 이벤트
+    const input_name = getElement("input-name");
+    input_name.addEventListener("keydown", function (event) {
+        if (event.key == "Enter") {
+            sendFindPwRequest();
+        }
+    });
+    // find_pw_button 클릭 이벤트
+    const find_pw_button = getElement("find-pw-button");
+    find_pw_button.addEventListener("click", sendFindPwRequest);
+}
+main();
